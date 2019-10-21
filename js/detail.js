ea8987db7c4eff1e9485743adc810d8161afa515
RockedPanda <chenghuawei@gmail.com>
2019-10-21 15:37:37 +0800

    初始化版本

---
+++ js/detail.js
@@ -0,0 +1,19 @@
+var vm = new Vue({
+    el:'#main',
+    data:function(){
+        return {
+            content: ''
+        };
+    },
+    methods: {
+        getTarget:function(item){
+            return item.type==='dir'?('index.html?path='+encodeURI(item.url)) : ('detail.html?path='+encodeURI(item.url));
+        }
+    },
+    mounted: function() {
+        var url = decodeURI(document.location.search.replace('?path=',''));
+        $.get(url).then(function(d){
+            vm.content = d;
+        });
+    },
+});
---
+++ js/main.js
@@ -0,0 +1,19 @@
+var vm = new Vue({
+    el:'#main',
+    data:function(){
+        return {
+            list: []
+        };
+    },
+    methods: {
+        getTarget:function(item){
+            return item.type==='dir'?('index.html?path='+encodeURI(item.url)) : ('detail.html?path='+encodeURI(item.download_url));
+        }
+    },
+    mounted: function() {
+        var url = decodeURI(document.location.search.replace('?path=','')) || 'https://api.github.com/repos/rockedpanda/kindel/contents';
+        $.get(url).then(function(d){
+            vm.list = d;
+        });
+    },
+});
